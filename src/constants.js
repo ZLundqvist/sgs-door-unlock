65ec14158cdfbf0b4b72bf761efb94831a5de2ed
Zack Lundqvist <Zack.lundqvist@hotmail.com>
2022-11-10 17:25:37 +0100

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ const UNLOCK_PARAMS = new URLSearchParams()
 UNLOCK_PARAMS.append('epName', 'Lindholmsallén 25 Gatan');
 
 module.exports = {
-    UNLOCK_URL: 'http://hemma.sgsstudentbostader.se/DoorControl/PerformUnlock',
+    UNLOCK_URL: 'http://hemma.sgs.se/DoorControl/PerformUnlock',
     UNLOCK_CONFIG: {
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
